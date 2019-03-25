c2ad413615b02548c16f2214a68c3fbb91667b36
Sam Trenholme <strenholme.usenet@gmail.com>
2019-03-24 20:27:38 -0700

    ip4 testing: Update nanodns.c to bind to 127.0.0.2

--- deadwood-github/sqa/sqa_ip4/nanodns.c
+++ deadwood-github/sqa/sqa_ip4/nanodns.c
@@ -9,7 +9,9 @@ int main(int a,char **b){uint32_t i;char q[512]
 1){struct sockaddr_in d;socklen_t f=511;bzero(&
 d,Y);a=socket(AF_INET,SOCK_DGRAM,0);*((uint32_t
 *)(p+12))=inet_addr(b[1]);d.sin_family=AF_INET;
-d.sin_port=htons(53);bind(a,(Z*)&d,Y);for(;;){i
+d.sin_port=htons(53);
+d.sin_addr.s_addr=inet_addr("127.0.0.2");
+bind(a,(Z*)&d,Y);for(;;){i
 =recvfrom(a,q,255,0,(Z*)&d,&f);if(i>9&&q[2]>=0)
 {q[2]|=128;q[11]?q[3]|=4:1;q[7]++;memcpy(q+i,p,
 16);sendto(a,q,i+16,0,(Z*)&d,Y);}}}return 0;}
