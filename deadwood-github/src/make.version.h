adbd6d9cf2531626a591f603e44d8ab916060bd1
Sam Trenholme <strenholme.usenet@gmail.com>
2020-06-02 06:47:01 -0700

    make.version.h: Make correct Deadwood version numbers
    For the record, here are the incorrect version numbers we had for
    earlier 3.5 releases of MaraDNS:
    
    3.5.0001: git-d498dc08141791194e4224e3159e0493bd52d447
    3.5.0002: git-db09084f07ca220bc392ebafb69a1c73dc3c5d1c
    3.5.0003: git-f17ad25edc4a18ff4adef3ba04055a8c9b6868ff
    3.5.0004: git-6d2ed571a49bd032cdeb03a73df06a8a27a217d5

--- deadwood-github/src/make.version.h
+++ deadwood-github/src/make.version.h
@@ -2,13 +2,24 @@
 
 # Make the version.h file based on the directory name for Deadwood
 
-# Now that this is in github, pull the version number from the git log
+# If we are a 3.5 release, use that to determine the version number
+if pwd | awk -F/ '{print $(NF - 1)}' | awk -F- '{print $2}
+		' | grep 3.5 > /dev/null ; then
+	pwd | awk -F/ '{print $(NF - 1)}' | awk -F- '
+		{print "#define VERSION \""$2"\""}' > version.h
+	exit 0
+fi
+
+# Otherwise, pull the version number from the git log
 if git log -1 > git.commit ; then
         head -1 git.commit | awk '
                 {print "#define VERSION \"git-"$NF"\""}' > version.h
         exit 0
 fi
 
+# This code was used by 3.4 and earlier releases; use it as a fallback
+# just in case we trigger the code.  We use the bogus version number 
+# 3.4.99 when testing for bugs.
 pwd | awk -F/ '{print $(NF - 1)}' | \
                 awk -F- '{
                 if($2 == "H" || $2 == "S" || $2 == "Q") {
