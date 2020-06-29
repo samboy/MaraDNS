adf5554ae4ecb6199de615c5153a58adbef64055
Sam Trenholme <strenholme.usenet@gmail.com>
2020-06-29 01:00:06 -0700

    Now that we allow leading whitespace, we can make parser smaller
    We can remove one state from the parser, which we used to use for
    the “no leading whitespace” rule.

--- deadwood-github/src/DwMararc.h
+++ deadwood-github/src/DwMararc.h
@@ -200,7 +200,7 @@ char *key_n_names[KEY_N_COUNT + 1] = {
 /* The actual state machine that we use to parse a MaraRC file; this is
  * described in the file doc/internals/MARARC.parser */
 
-#define dwm_machine "a Hb Y1c Wxb Rxp T;\n" \
+#define dwm_machine "a Hb Y1c Wa Rxp T;\n" \
                     "b Xb Rxp T;\n" \
                     "c B1c Wd =e [f +g (y\n" \
                     "d Wd =e [f +g\n" \
@@ -224,8 +224,7 @@ char *key_n_names[KEY_N_COUNT + 1] = {
                     "xp T;\n" \
                     "y Qz\n" \
                     "z I7z Qxa\n" \
-                    "xa )k\n" \
-                    "xb Hb Wxb Y1c Rxp T;\n"
+                    "xa )k\n" 
 
 /* A tokenized single state in the finite state machine */
 typedef struct {
