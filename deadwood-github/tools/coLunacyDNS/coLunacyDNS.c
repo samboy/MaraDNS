efb9da10c7944ecc5c02cefdf4945d4b47ba0cb6
Sam Trenholme <strenholme.usenet@gmail.com>
2020-08-09 09:38:08 -0700

    coLunacyDNS: Update version number

--- deadwood-github/tools/coLunacyDNS/coLunacyDNS.c
+++ deadwood-github/tools/coLunacyDNS/coLunacyDNS.c
@@ -1738,7 +1738,7 @@ int main(int argc, char **argv) {
 	SipHashSetKey(rand64(),rand64());
 
         if(argc != 2 || *argv[1] == '-') {
-                printf("coLunacyDNS version 2020-08-08 starting\n\n");
+                printf("coLunacyDNS version 2020-08-09 starting\n\n");
         }
         set_time(); // Run this frequently to update timestamp
         // Get bindIp and returnIp from Lua script
@@ -2032,7 +2032,7 @@ int main(int argc, char **argv) {
                         svc_install_service();
                 }
         } else {
-                printf("coLunacyDNS version 2020-08-08\n\n");
+                printf("coLunacyDNS version 2020-08-09\n\n");
                 printf(
                     "coLunacyDNS is a DNS server that is a Windows service\n\n"
                     "To install this service:\n\n\tcoLunacyDNS --install\n\n"
