d56fddc4944fc91707c12891d61812af63fd243a
Sam Trenholme <set@sublime.lan.samiam.org>
2020-08-15 02:37:21 -0700

    coLunacyDNS: Getting closer to IPv6 socket support

--- deadwood-github/tools/coLunacyDNS/compile.coLunacyDNS.sh
+++ deadwood-github/tools/coLunacyDNS/compile.coLunacyDNS.sh
@@ -8,5 +8,5 @@ cd ../tools/coLunacyDNS/
 FLAGS="-Os -Wall"
 gcc $FLAGS -c -o coLunacyDNS.o coLunacyDNS.c
 gcc $FLAGS -o coLunacyDNS coLunacyDNS.o ../../lunacy/liblua.a -lm
-gcc $FLAGS -c -o HalfSipHash1-3.o halfSipHash1-3.c
+gcc $FLAGS -c -o HalfSipHash1-3.o HalfSipHash1-3.c
 gcc $FLAGS -o HalfSipHash1-3 HalfSipHash1-3.o ../../lunacy/liblua.a -lm
