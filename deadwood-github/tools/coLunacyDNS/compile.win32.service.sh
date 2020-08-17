564fc1d01fbb86587acf62e851810760072e19d9
Sam Trenholme <strenholme.usenet@gmail.com>
2020-08-17 11:50:46 -0700

    Ugh.  MINGW still can use use standard Unix IPv6 sockets.

--- deadwood-github/tools/coLunacyDNS/compile.win32.service.sh
+++ deadwood-github/tools/coLunacyDNS/compile.win32.service.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
 
-if [ ! -e /mingw ] ; then
-	echo Only run this in Mingw
-	exit 1
-fi
+#if [ ! -e /mingw ] ; then
+#	echo Only run this in Mingw
+#	exit 1
+#fi
 
 # No symlinks
-rm -f lauxlib.h lua.h luaconf.h lualib.h
+#rm -f lauxlib.h lua.h luaconf.h lualib.h
 
-for a in lauxlib.h lua.h luaconf.h lualib.h ; do
-	cp ../../lunacy/$a .
-done
+#for a in lauxlib.h lua.h luaconf.h lualib.h ; do
+#	cp ../../lunacy/$a .
+#done
 
 cd ../../lunacy/
-make
+mv Makefile Makefile.disabled
+cp Makefile.mingw64 Makefile
+make clean
+export CC="x86_64-w64-mingw32-gcc"
+make -f Makefile.mingw64
+mv Makefile.disabled Makefile
 cd ../tools/coLunacyDNS/
-gcc -Wall -Os -DNOIP6 -DMINGW -c -o coLunacyDNS.o coLunacyDNS.c
-gcc -Os -o coLunacyDNS coLunacyDNS.o ../../lunacy/liblua.a -lm -lwsock32
+echo $CC -Wall -O3 -DMINGW -c -o coLunacyDNS.o coLunacyDNS.c
+$CC -Wall -O3 -DMINGW -c -o coLunacyDNS.o coLunacyDNS.c
+$CC -O3 -o coLunacyDNS.exe coLunacyDNS.o ../../lunacy/liblua.a -lm -lwsock32
+#gcc -Wall -Os -DNOIP6 -DMINGW -c -o coLunacyDNS.o coLunacyDNS.c
+#gcc -Os -o coLunacyDNS coLunacyDNS.o ../../lunacy/liblua.a -lm -lwsock32
