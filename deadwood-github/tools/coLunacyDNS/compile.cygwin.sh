32dda34129f255e827f48ebb86b750fc43725436
Sam Trenholme <git@samiam.org>
2020-08-21 23:48:01 -0700

    git rm compile.cygwin.sh (We now have Makefile.cygwin)

--- deadwood-github/tools/coLunacyDNS/compile.cygwin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# With only one file, I will not bother with a Makefile
-
-cd ../../lunacy/
-make 
-cd ../tools/coLunacyDNS/
-gcc -DCYGWIN -g -c -o coLunacyDNS.o coLunacyDNS.c
-gcc -g -o coLunacyDNS coLunacyDNS.o ../../lunacy/liblua.a -lm
