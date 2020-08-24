b790c7d44782dbdef6cfe00d571a3417f9a1d455
Sam Trenholme <git@samiam.org>
2020-08-23 18:48:29 -0700

    lua.h: Bump version number with warnings fixed, mkstemp change

--- deadwood-github/lunacy/lua.h
+++ deadwood-github/lunacy/lua.h
@@ -18,7 +18,7 @@
 
 
 #define LUA_VERSION     "Lunacy 5.1"
-#define LUA_RELEASE     "Lunacy 2020-08-11"
+#define LUA_RELEASE     "Lunacy 2020-08-23"
 #define LUA_VERSION_NUM 501
 #define LUA_COPYRIGHT   "Copyright (C) 1994-2012 Lua.org, PUC-Rio, 2020 Sam Trenholme"
 #define LUA_AUTHORS     "R. Ierusalimschy, L. H. de Figueiredo, W. Celes, Sam Trenholme"
