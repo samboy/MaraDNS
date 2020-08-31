3ab7e275ccd04c2382b3b6684cbc3eefac30c6b1
Sam Trenholme <git@samiam.org>
2020-08-31 00:04:17 +0000

    coLunacyDNS sqa: Coverage is now at 100%  1.0.008 release.

--- deadwood-github/tools/coLunacyDNS/coLunacyDNS.c
+++ deadwood-github/tools/coLunacyDNS/coLunacyDNS.c
@@ -2083,7 +2083,7 @@ int main(int argc, char **argv) {
 	SipHashSetKey(rand32(),rand32());
 
         if(argc != 2 || *argv[1] == '-') {
-                printf("coLunacyDNS version 1.0.00X starting\n\n");
+                printf("coLunacyDNS version 1.0.008 starting\n\n");
         }
         set_time(); // Run this frequently to update timestamp
         // Get bindIp and returnIp from Lua script
@@ -2391,7 +2391,7 @@ int main(int argc, char **argv) {
                         svc_install_service();
                 }
         } else {
-                printf("coLunacyDNS version 1.0.00X\n\n");
+                printf("coLunacyDNS version 1.0.008\n\n");
                 printf(
                     "coLunacyDNS is a DNS server that is a Windows service\n\n"
                     "To install this service:\n\n\tcoLunacyDNS --install\n\n"
