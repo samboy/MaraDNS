3cc7ab6bbf8a9823234e01945cd6ca9fb108260b
Sam Trenholme <maradns@samiam.org>
2020-12-12 22:41:51 -0800

    MaraDNS Dockerfile: CentOS 8 → Ubuntu 20.04
    See https://github.com/samboy/MaraDNS/issues/91

--- Docker-stuff/make.docker.image.sh
+++ Docker-stuff/make.docker.image.sh
@@ -6,12 +6,4 @@
 # during testing, but MaraDNS itself is pulled from GitHub in
 # the image to run the actual tests.
 
-## CENTOS 8 NOTE ##
-# Add these lines to /etc/security/limits.conf (without hashtag) so this
-# works
-#*       soft    nofile  131072
-#*       hard    nofile  131072
-#*       soft    nproc   131072
-#*       hard    nproc   131072
-
 docker build -t maradns-$( date +%Y-%m-%d ) .
