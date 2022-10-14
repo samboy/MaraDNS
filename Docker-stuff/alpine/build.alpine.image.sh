7bb20d9a2eb3c5ed19463127bf7a97012a09ee33
Sam Trenholme <maradns@samiam.org>
2022-10-13 19:52:37 -0700

    Alpine Dockerfile: Add MaraDNS stuff, rg32hash, valgrind, clang, etc.

--- Docker-stuff/alpine/build.alpine.image.sh
+++ Docker-stuff/alpine/build.alpine.image.sh
@@ -2,6 +2,9 @@
 
 # Build an alpine image which can test MaraDNS
 
+cp ../rg32hash.tar.gz .
+cp ../run.tests.sh .
 docker build -t alpine-$( date +%Y-%m-%d ) .
+rm -f rg32hash.tar.gz run.tests.sh
 
 # Work in progress: We still need to add the testing scripts, etc.
