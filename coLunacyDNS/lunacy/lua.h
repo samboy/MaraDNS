7feb9644d32252a0382a5197934b2fdee96c2940
Sam Trenholme <maradns@samiam.org>
2022-10-22 19:11:09 -0700

    lunacy lua.h: Update year of copyright

--- coLunacyDNS/lunacy/lua.h
+++ coLunacyDNS/lunacy/lua.h
@@ -20,7 +20,7 @@
 #define LUA_VERSION     "Lunacy 5.1"
 #define LUA_RELEASE     "Lunacy 20201206p2"
 #define LUA_VERSION_NUM 501
-#define LUA_COPYRIGHT   "Copyright (C) 1994-2012 Lua.org, PUC-Rio, 2020 Sam Trenholme"
+#define LUA_COPYRIGHT   "Copyright 1994-2012 Lua.org PUC-Rio; 2020-2022 Sam Trenholme"
 #define LUA_AUTHORS     "R. Ierusalimschy, L. H. de Figueiredo, W. Celes, Sam Trenholme"
 
 
