52a667ef9c248adc7f3a8797304217d05187d022
Sam Trenholme <maradns@samiam.org>
2022-11-07 07:11:45 -0800

    Update Dockerfiles to use maramake
    Now, all MaraDNS building and testing is done with a mostly
    pure POSIX make instead of GNU make, to avoid GNUisms with the
    build/make process.

--- Docker-stuff/alpine/build.alpine.image.sh
+++ Docker-stuff/alpine/build.alpine.image.sh
@@ -3,6 +3,7 @@
 # Build an alpine image which can test MaraDNS
 
 cp ../rg32hash.tar.gz .
+cp ../maramake-2022-11-01.tar.gz .
 cp ../run.tests.sh .
 docker build -t alpine-$( date +%Y-%m-%d ) .
 rm -f rg32hash.tar.gz run.tests.sh
