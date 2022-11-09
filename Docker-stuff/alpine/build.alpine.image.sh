a549436e73b651f279266ee00d9aefe8b2ea5563
Sam Trenholme <maradns@samiam.org>
2022-11-09 09:36:40 -0800

    Docker updates: Remove GNU make, etc.
    * GNU make removed from Ubuntu and Alpine Dockerfiles (we now use maramake)
    * Better cleanup after building Alpine image
    * Renamed make.docker.image.sh → build.ubuntu.image.sh

--- Docker-stuff/alpine/build.alpine.image.sh
+++ Docker-stuff/alpine/build.alpine.image.sh
@@ -6,6 +6,6 @@ cp ../rg32hash.tar.gz .
 cp ../maramake-2022-11-01.tar.gz .
 cp ../run.tests.sh .
 docker build -t alpine-$( date +%Y-%m-%d ) .
-rm -f rg32hash.tar.gz run.tests.sh
+rm -f rg32hash.tar.gz run.tests.sh maramake-2022-11-01.tar.gz
 
 # Work in progress: We still need to add the testing scripts, etc.
