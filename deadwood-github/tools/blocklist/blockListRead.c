c9dae565478ba23253dddde0d24132c691de3742
Sam Trenholme <maradns@samiam.org>
2022-12-02 00:54:45 -0800

    Begin code to read the block list file
    (I should call it a “naughty list” in Deadwood, so we can also have a
     “nice list”.)

---
+++ deadwood-github/tools/blocklist/blockListRead.c
@@ -0,0 +1,30 @@
+/* Copyright (c) 2022 Sam Trenholme
+ *
+ * TERMS
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * This software is provided 'as is' with no guarantees of correctness or
+ * fitness for purpose.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdint.h>
+
+int main(int argc, char **argv) {
+  stuct stat get;
+  if(stat("bigBlock.bin",&get) == -1) {
+    return 1; // Error
+  }
+  printf("%d\n",get.st_size);
+}
