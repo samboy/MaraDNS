5f58b7254f1359421e429bde0cdad58366729ec9
Sam Trenholme <maradns@samiam.org>
2022-12-02 01:57:27 -0800

    blockListRead: Add HalfSip13 function

--- deadwood-github/tools/blocklist/blockListRead.c
+++ deadwood-github/tools/blocklist/blockListRead.c
@@ -22,6 +22,77 @@
 #include <stdint.h>
 #include <stdio.h>
 
+typedef struct {
+  uint8_t *block;
+  uint32_t max;
+  uint32_t sipKey1;
+  uint32_t sipKey2;
+  int32_t hashSize;
+} blockHash;
+
+uint32_t HalfSip13(uint8_t *str, int32_t l, blockHash *bHash) {
+  if(bHash == NULL || str == NULL) { 
+    return 0;
+  }
+  size_t offset = 0;
+  uint32_t v0, v1, v2, v3, m;
+  int shift = 0, round = 0;
+
+  v0 = bHash->sipKey1;
+  v1 = bHash->sipKey2;
+  v2 = v0 ^ 0x6c796765;
+  v3 = v1 ^ 0x74656462;
+  m = 0;
+  while(offset <= l) {
+    if(offset < l) {
+      m |= (uint32_t)(str[offset] & 0xff) << shift;
+      shift += 8;
+    }
+    while(shift >= 32 || offset == l) { // "while" to avoid goto
+      if(offset == l && shift != 32) {
+        m |= (uint64_t)(l & 0xff) << 24;
+        offset++;
+      }
+      shift = 0;
+      v3 ^= m;
+
+      v0 += v1;
+      v1 = (v1 << 5) | (v1 >> 27);
+      v1 ^= v0;
+      v0 = (v0 << 16) | (v0 >> 16);
+      v2 += v3;
+      v3 = (v3 << 8) | (v3 >> 24);
+      v3 ^= v2; v0 += v3;
+      v3 = (v3 << 7) | (v3 >> 25);
+      v3 ^= v0; v2 += v1;
+      v1 = (v1 << 13) | (v1 >> 19);
+      v1 ^= v2;
+      v2 = (v2 << 16) | (v2 >> 16);
+
+      v0 ^= m;
+      shift = 0;
+      m = 0;
+    }
+    offset++;
+  }
+  v2 ^= 255;
+  for(round = 0; round < 3; round++) {
+    v0 += v1;
+    v1 = (v1 << 5) | (v1 >> 27);
+    v1 ^= v0;
+    v0 = (v0 << 16) | (v0 >> 16);
+    v2 += v3;
+    v3 = (v3 << 8) | (v3 >> 24);
+    v3 ^= v2; v0 += v3;
+    v3 = (v3 << 7) | (v3 >> 25);
+    v3 ^= v0; v2 += v1;
+    v1 = (v1 << 13) | (v1 >> 19);
+    v1 ^= v2;
+    v2 = (v2 << 16) | (v2 >> 16);
+  }
+  return v1 ^ v3;
+}
+
 int main(int argc, char **argv) {
   struct stat get;
   if(stat("bigBlock.bin",&get) == -1) {
