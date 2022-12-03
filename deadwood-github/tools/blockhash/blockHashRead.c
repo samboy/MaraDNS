a8c09e00384180f69328670e7d3ba590277df209
Sam Trenholme <maradns@samiam.org>
2022-12-03 13:56:24 -0800

    Add blockHasString test
    This is a fast test to see if a given string is in a hash block.
    
    This will be used by Deadwood for compact “naughty” (blocked domain) lists.

--- deadwood-github/tools/blockhash/blockHashRead.c
+++ deadwood-github/tools/blockhash/blockHashRead.c
@@ -113,6 +113,61 @@ uint32_t HalfSip13(uint8_t *str, int32_t l,
   return v1 ^ v3;
 }
 
+// See if a black has a given binary string (string str, length len)
+// 1: Yes, it does
+// 0: No, it does not
+// -1: An error happened when trying to find the string
+int blockHasString(blockHash *b, uint8_t *str, int32_t len) {
+  uint32_t sipHashBucket;
+  uint32_t offset;
+  if(b == NULL || str == NULL) {
+    return -1; // Error
+  }
+  sipHashBucket = HalfSip13(str, len, b->sipKey1, b->sipKey2) % b->hashSize;
+  offset = read32bitNumber(b->block, 16 + (4 * sipHashBucket), b->max); 
+  if(offset == 0) { 
+    return 0; // Not found: No strings for this hash bucket
+  }
+  if(offset >= b->max - 1) { // - 1 because we will read 16-bit number
+    return -1; // Error
+  }
+  while(offset < b->max) {
+    int32_t stringLen, strOffset;
+    uint32_t strEnd;
+    stringLen = b->block[offset];
+    if(stringLen == 0xff) { // End of hash chain
+      return 0; // Not found: End of hash bucket chain reached
+    }
+    offset++;
+    stringLen <<= 8;
+    if(offset >= b->max - 1) { return -1; } // - 1 to avoid 2nd bounds check
+    stringLen |= b->block[offset];
+    if(stringLen < 0 || stringLen > 0xff00) { return -1; }
+    offset++; 
+    strEnd = offset + stringLen;
+    if(strEnd > b->max) { return -1; }
+    if(stringLen != len) { // Different length, try next string
+      offset = strEnd;
+      continue;
+    }
+    strOffset = 0;
+    while(stringLen > 0) {
+      if(b->block[offset] != str[strOffset]) { // Different string, try next
+        offset = strEnd;
+        break;
+      }
+      stringLen--;
+      strOffset++;
+      offset++;
+   }
+   if(stringLen == 0) {
+     return 1; // Strings match, found
+   }
+ }
+ return -1; // We should never get here, error   
+}
+
+// Read a file and make a blockHash structure
 blockHash *makeBlockHash(char *filename) {
   blockHash *out = NULL;
   int fileDesc;
@@ -150,5 +205,12 @@ int main(int argc, char **argv) {
   printf("%p %d %08x %08x %d\n",globalBlockHash->block, globalBlockHash->max,
       globalBlockHash->sipKey1, globalBlockHash->sipKey2, 
       globalBlockHash->hashSize);
+  char *testString = "\3www\4fejs\2ml\0";
+  int testLength = 13;
+  printf("www.fejs.ml test result should be 1: %d\n",
+      blockHasString(globalBlockHash,(uint8_t *)testString, testLength));
+  char *testString2 = "\3www\4fexs\2ml\0";
+  printf("www.fexs.ml test result should be 0: %d\n",
+      blockHasString(globalBlockHash,(uint8_t *)testString2, testLength));
   return 0;
 }
