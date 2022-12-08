8b6b50c47eb9e6fcb7b48ae32199d2edc5bf736a
Sam Trenholme <maradns@samiam.org>
2022-12-08 11:53:20 -0800

    coLunacyDNS: Update HalfSip1-3 test
    The test now runs the same test as used by the Deadwood code,
    and has the same output as seen in the test vectors generated
    by the reference HalfSipHash code.

--- coLunacyDNS/HalfSipHash13.c
+++ coLunacyDNS/HalfSipHash13.c
@@ -9,8 +9,17 @@ int main() {
 	char test[66];
         int a;
 	SipHashSetKey(0x03020100, 0x07060504);
+	printf("Test #1: Reference vectors\n");
+	printf("See https://github.com/samboy/HalfSipTest for ref code\n");
         for(a = 0; a < 64; a++) { 
 		test[a] = a;
 		printf("%08x\n",SipHash(test, a)); 
 	}
+	SipHashSetKey(0xfcfdfeff, 0xf8f9fafb);
+	printf("Test #2: Inverted reference vectors\n");
+        for(a = 0; a < 64; a++) {
+                test[a] = a ^ 0xff;
+                printf("%08x\n",
+                         SipHash(test,a));
+        }
 }
