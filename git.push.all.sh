d8aaa2950b762014aacb759fd2598f2a4669170f
Sam Trenholme <strenholme.usenet@gmail.com>
2026-04-18 17:26:45 -0600

    Add script to push everywhere (Gitlab is annoying)

--- git.push.all.sh
+++ git.push.all.sh
@@ -1,8 +1,28 @@
 #!/bin/sh
 
-git push origin
-git push sourcehut
-git push bitbucket
-git push codeberg
-git push gitlab # Fix your site to work with Firefox!
-git push sourceforge
+for a in origin sourcehut bitbucket codeberg sourceforge ; do
+  echo $a
+  git push $a
+  echo
+done
+
+echo Gitlab annoyance: I had to use Chrome to update my SSH key
+git push gitlab
+echo
+
+cat > /dev/null << EOF
+[remote "origin"]
+        url = git@github.com:samboy/MaraDNS.git
+        fetch = +refs/heads/*:refs/remotes/origin/*
+[remote "sourcehut"]
+        url = git@git.sr.ht:~samiam/MaraDNS
+[remote "bitbucket"]
+        url = git@bitbucket.org:maradns/maradns.git
+[remote "codeberg"]
+        url = git@codeberg.org:samboy/MaraDNS.git
+[remote "gitlab"]
+        url = git@gitlab.com:maradns/maradns.git
+[remote "sourceforge"]
+        url = ssh://samboy@git.code.sf.net/p/maradns-git/code
+EOF
+
