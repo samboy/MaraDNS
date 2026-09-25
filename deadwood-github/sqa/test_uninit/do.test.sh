22767b72ece65a7033a22afe0df10ecd8ab306c3
Sam Trenholme <git@samiam.org>
2026-09-25 18:57:20 +0000

    do.test.sh: Remove full path to compiler if set
    I checked, and this is POSIX2024 compliant

--- deadwood-github/sqa/test_uninit/do.test.sh
+++ deadwood-github/sqa/test_uninit/do.test.sh
@@ -6,7 +6,7 @@ for COMPILER in tcc gcc clang ; do
     VERSION=$( $COMPILER --version | head -1 )
     for FLAGS in -g -O -O1 -O2 -O3 ; do
       $COMPILER -std=c99 $FLAGS -o test test.c
-      echo -n $SYSTEM $COMPILER $VERSION ${FLAGS} ' - '
+      echo -n $SYSTEM ${COMPILER##*/} $VERSION ${FLAGS} ' - '
       ./test | awk -f verify.awk
       rm -f test test.exe
     done
