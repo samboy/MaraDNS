0e7285e8452989671704f309111af559097c5473
Sam Trenholme <git@samiam.org>
2026-09-25 13:40:27 -0600

    test_uinit: Tests against -Os and no flags

--- deadwood-github/sqa/test_uninit/do.test.sh
+++ deadwood-github/sqa/test_uninit/do.test.sh
@@ -4,11 +4,16 @@ SYSTEM=$( uname -a )
 for COMPILER in tcc gcc clang ; do
   if command -v $COMPILER > /dev/null 2>&1 ; then
     VERSION=$( $COMPILER --version | head -1 )
-    for FLAGS in -g -O -O1 -O2 -O3 ; do
+    for FLAGS in -g -O -Os -O1 -O2 -O3 ; do
       $COMPILER -std=c99 $FLAGS -o test test.c
       echo -n $SYSTEM ${COMPILER##*/} $VERSION ${FLAGS} ' - '
       ./test | awk -f verify.awk
       rm -f test test.exe
     done
+    # No flags
+    $COMPILER -std=c99 -o test test.c
+    echo -n $SYSTEM ${COMPILER##*/} $VERSION \(No flags\) ' - '
+    ./test | awk -f verify.awk
+    rm -f test test.exe
   fi
 done
